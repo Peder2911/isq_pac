c88fe227d29f8dfa8e2dbd2040744627c1545381
peder2911 <pglandsverk@gmail.com>
2020-06-22 11:00:56 +0200

    Changes to table A-1

--- tablescripts/2018_ct.R
+++ tablescripts/2018_ct.R
@@ -17,16 +17,47 @@ data <- data %>%
       minor_prob + major_prob > 0.1 ~ "c",
       TRUE ~ "d"
    )) %>%
+   group_by(cat) %>%
    summarize(
-      `p(major) > 0.1` = sum(cat == "a"),
-      `p(minor) > 0.3 & p(major) < 0.1` = sum(cat == "b"),
-      `p(minor) + p(major) > 0.1` = sum(cat == "c"),
-      `p(minor) + p(major) < 0.1` = sum(cat == "d")
-   ) %>%
-   gather(Class,Count)
+      none = n()-sum(either_actual),
+      minor = sum(minor_actual),
+      major = sum(major_actual),
+      cnt = n()
+   )
 
+data <- data.frame(rbind(
+   data,
+   c("",sapply(data[2:ncol(data)],sum))
+))
+
+data$cat <- c(
+   "p(major) > 0.1",
+   "p(minor) > 0.3 & p(major) < 0.1",
+   "p(minor) + p(major) > 0.1",
+   "p(minor) + p(major) < 0.1",
+   "Sum"
+)
+
+names(data) <- c(
+   "Category",
+   "No Conflict",
+   "Minor conflict",
+   "Major conflict",
+   "Total"
+)
+
+#   )) %>%
+#   summarize(
+#      `p(major) > 0.1` = sum(cat == "a"),
+#      `p(minor) > 0.3 & p(major) < 0.1` = sum(cat == "b"),
+#      `p(minor) + p(major) > 0.1` = sum(cat == "c"),
+#      `p(minor) + p(major) < 0.1` = sum(cat == "d")
+#   ) %>%
+#   gather(Class,Count)
+
+write.csv(data,"/tmp/dat.csv")
 kable(data,"rst")
-sum(data[,2])
+#sum(data[,2])
 
 kable(data,"latex", booktabs = TRUE) %>%
    stripTableEnvir() %>%
